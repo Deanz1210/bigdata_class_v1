2e9e07d7ec6b793732d59e7828aeaf6652c0218e
Deanz1210 <dean40253@gmail.com>
2025-03-18 11:56:11 +0800

    3/18

--- resources/w4/w04-2-Analyze top keywords and build Django website/website-news-analysis-v1-app-top-keyword/website_configs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
-urlpatterns = [
-    # top keywords
-    path('topword/', include('app_top_keyword.urls'))
-]
