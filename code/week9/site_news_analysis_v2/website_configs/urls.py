613735ad9ae8085ed9a474523e235c8ce02c833e
Dean <dean40253@gmail.com>
2025-04-28 11:03:25 +0800

    4/28/2

--- code/week9/site_news_analysis_v2/website_configs/urls.py
+++ code/week9/site_news_analysis_v2/website_configs/urls.py
@@ -1,34 +1,19 @@
-"""
-URL configuration for website_configs project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from app_game_mayor import views as mayor_views  # <<== 新增這行，取別名 mayor_views
 
 urlpatterns = [
-   #path('admin/', admin.site.urls),
-   path('topword/', include('app_top_keyword.urls')),
-   path('topperson/', include('app_top_person.urls')),
+    path('', mayor_views.home, name='home'),  # <<== 設定首頁，home 由 app_game_mayor.views.home 控制
+    path('api_get_game_mayor_data/', mayor_views.api_get_game_mayor_data, name='api_get_game_mayor_data'),  # <<== 新增 API 路由
+
+    path('topword/', include('app_top_keyword.urls')),
+    path('topperson/', include('app_top_person.urls')),
     path('userkeyword/', include('app_user_keyword.urls')),
-   path('hotpersonsofyesterday/', include('app_hot_persons_of_yesterday.urls', namespace='app_hot_person_of_yesterday')),
+    path('hotpersonsofyesterday/', include('app_hot_persons_of_yesterday.urls', namespace='app_hot_person_of_yesterday')),
     path('voiceofshare/', include('app_voice_of_share.urls')),
     path('wordcloud/', include('app_word_cloud.urls')),
     path('correlation/', include('app_correlation_analysis.urls')),
     path('userkeyword_assoc/', include('app_user_keyword_association.urls')),
     path('userkeyword_senti/', include('app_user_keyword_sentiment.urls')),
     path('gamemayor/', include('app_game_mayor.urls')),
-    
 ]
